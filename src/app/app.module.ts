6cfe627f456d71e93417030ec170d6b9d6c0149a
AlexanderTishchenko <sashoktishchenko@gmail.com>
2019-12-12 16:38:03 +0300

    First commit on app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {HeaderModule} from "./header/header.module";
+import {BarTableModule} from "./bar-table/bar-table.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HeaderModule,
+    BarTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
