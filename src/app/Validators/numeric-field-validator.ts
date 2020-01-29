91709c32312211b9ef60f0c6d421f767f3143a44
a.tyshchanka <sashoktishchenko@gmail.com>
2020-01-29 16:26:41 +0300

    simple fix commit

--- src/app/Validators/numeric-field-validator.ts
+++ src/app/Validators/numeric-field-validator.ts
@@ -1,7 +1,9 @@
 import {FormControl, ValidationErrors} from "@angular/forms";
 
 export function numericFieldValidator(control: FormControl): ValidationErrors {
-  if (control.value === null) return null;
+  if (control.value === null) {
+    return null;
+  }
   if (isNaN(control.value)) {
     return { numericValidatorWarning: 'Значение должно быть числом!' };
   }
